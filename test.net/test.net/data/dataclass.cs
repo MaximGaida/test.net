668770a3c568185cd6600f8f75a5b803a60bdd32
mymac <maxim.gaida@isa.utm.md>
2022-12-14 21:51:16 +0200

    add 4 test

---
+++ test.net/test.net/data/dataclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace test.net.data
+{
+	public class dataclass
+	{
+		
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
+
---
+++ test.net/testingnet/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using test.net.data;
+
+namespace testingnet
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingAddTest()
+        {
+            Assert.Equal(4, dataclass.Add(2, 2));
+        }
+
+
+
+        [Fact]
+        public void PassingAddTest1()
+        {
+            Assert.Equal(6, dataclass.Add(3, 3));
+        }
+
+        [Fact]
+        public void PassingAddTest2()
+        {
+            Assert.Equal(8, dataclass.Add(4, 4));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.NotEqual(5, dataclass.Add(2, 2));
+        }
+    }
+
+
+}
+////
